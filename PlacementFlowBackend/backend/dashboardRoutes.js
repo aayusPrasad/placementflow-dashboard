fd0e0e95ec322c65bf68ffe563ca02c223d6d87b
Aayush_Prasad <166906326+aayusPrasad@users.noreply.github.com>
2026-05-07 12:43:27 +0530

    Delete PlacementFlowBackend/backend directory

--- PlacementFlowBackend/backend/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { getRecruiterDashboard } = require('./dashboardController');
-const authMiddleware = require('./authMiddleware');
-
-router.get('/', authMiddleware, getRecruiterDashboard);
-
-module.exports = router;
